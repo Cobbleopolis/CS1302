42cd02978ac27073382561cdd275add7c1386935
Cobbleopolis <cobbleopolis@gmail.com>
2017-03-07 16:00:18 -0500

    Updated zipProject bat file

--- zipProject.bat
+++ zipProject.bat
@@ -2,11 +2,6 @@
 
 SET E_CODE=0
 
-IF [%1] == [] (
-    SET E_CODE=1
-    GOTO EXIT
-)
-
 SET ZIP_SUFFIX=LoganThompson-Section8
 SET SEVEN_Z="C:\Program Files\7-Zip\7z"
 SET ZIP_NAME=%1-%ZIP_SUFFIX%.zip
@@ -15,14 +10,9 @@ CD %1
 
 IF EXIST %ZIP_NAME% DEL %ZIP_NAME%
 
-IF EXIST %ZIP_NAME% (
-    SET E_CODE=2
-    GOTO EXIT
-)
-
 %SEVEN_Z% a %ZIP_NAME% src -r -tzip
 %SEVEN_Z% a %ZIP_NAME% output -r -tzip
+%SEVEN_Z% a %ZIP_NAME% outputs -r -tzip
 %SEVEN_Z% a %ZIP_NAME% input -r -tzip
-LABEL Exit
+%SEVEN_Z% a %ZIP_NAME% inputs -r -tzip
 CD %~dp0
-EXIT /B %E_CODE%
