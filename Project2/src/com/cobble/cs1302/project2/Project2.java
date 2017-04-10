24fca7136bf3df39ec9bb391aa49ce311c3a1169
Cobbleopolis <cobbleopolis@gmail.com>
2017-04-10 16:51:53 -0400

    Added project 2

---
+++ Project2/src/com/cobble/cs1302/project2/Project2.java
@@ -0,0 +1,21 @@
+//Class:      CS1301/08
+//Term:       Spring 2017
+//Name:       Logan Thompson
+//Instructor: Monisha Verma
+//Assignment: Project 2 | Option 2
+package com.cobble.cs1302.project2;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Project2 extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
